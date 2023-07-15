4529b99dbf728cdad9531d2257b2147903ac5242
TearGosling <teargoslingwaifu@gmail.com>
2023-07-14 20:42:00 -0500

    Rewrite MeZO to be its own class

--- training/mezo.py
+++ training/mezo.py
@@ -69,6 +69,132 @@ OPTIMIZER_NAME = "optimizer.pt"
 SCHEDULER_NAME = "scheduler.pt"
 SCALER_NAME = "scaler.pt"
 
+IGNORE_DECAY_PARAMS = ["bias", "layer_norm", "layernorm"]
+
+class MeZOOptimizer:
+    def __init__(self, trainer: Trainer, model: nn.Module) -> None:
+        self.trainer = trainer
+        self.batch_size = trainer._train_batch_size
+        self.eps = trainer.zo_eps
+        self.model = model
+
+        assert trainer.args.gradient_accumulation_steps == 1, "MeZO does not support gradient accumulation!"
+        # Initialize named_parameters_to_optim
+        self.named_parameters_to_optim = []
+        for name, param in model.named_parameters():
+            if param.requires_grad:
+                self.named_parameters_to_optim.append((name, param))
+
+    def step(self, inputs: dict) -> torch.Tensor:
+        args = self.trainer.args
+        # Reset seeds, gradients and losses
+        self.losses = []
+        self.projected_grads = []
+        self.random_seeds = []
+
+        # Do a step for every entry in the batch
+        for i in range(self.batch_size):
+            # Fetch input from data, unsqueezing necessary because model wants tensor of shape
+            # (batch_size, context_len)
+            entry = {
+                "input_ids": inputs['input_ids'][i].unsqueeze(0),
+                "labels": inputs['labels'][i].unsqueeze(0),
+            }
+            # Generate seed for sampling z
+            seed = np.random.randint(1000000000)
+
+            # First function evaluation
+            self._perturb_parameters(random_seed=seed, scaling_factor=1)
+            loss1 = self._forward_pass(entry)
+
+            # Second function evaluation
+            self._perturb_parameters(random_seed=seed, scaling_factor=-2)
+            loss2 = self._forward_pass(entry)
+
+            # Calculate and normalize grad
+            self.projected_grad = ((loss1 - loss2) / (2 * self.eps)).item()
+            self.projected_grad = self._log_normalize(self.projected_grad)
+
+            # Reset model back to its parameters at start of step
+            self._perturb_parameters(random_seed=seed, scaling_factor=1)
+
+            # Append things
+            self.losses.append(loss1)
+            self.projected_grads.append(self.projected_grad)
+            self.random_seeds.append(seed)
+
+        # Return the average of the original losses
+        return torch.mean(torch.tensor(self.losses, device=args.device, dtype=torch.float32, requires_grad=False))
+
+    def update(self) -> None:
+        '''Updates the parameters.'''
+        args = self.trainer.args
+
+        for i in range(self.batch_size):
+            # Reset the random seed for sampling zs
+            torch.manual_seed(self.random_seeds[i])           
+
+            for name, param in self.named_parameters_to_optim:
+                # Resample z
+                z = self._sample_z(param)
+                if _list_not_in_name(search_terms=IGNORE_DECAY_PARAMS, name=name):
+                    param.data = param.data - (self.trainer._get_learning_rate() / self.batch_size) * (self.projected_grads[i] * z + args.weight_decay * param.data)
+                else:
+                    param.data = param.data - (self.trainer._get_learning_rate() / self.batch_size) * (self.projected_grads[i] * z)
+        
+        self.trainer.lr_scheduler.step()
+
+    def _forward_pass(self, inputs: dict) -> torch.Tensor:
+        self.model.eval()
+        
+        with torch.inference_mode():
+            inputs = self.trainer._prepare_inputs(inputs)
+            with self.trainer.compute_loss_context_manager():
+                loss = self.trainer.compute_loss(self.model, inputs)
+            if self.trainer.args.n_gpu > 1:
+                # Warning: this is copied from the original HF Trainer. Untested.
+                loss = loss.mean() # mean() to average on multi-gpu parallel training
+            
+        return loss.detach()
+
+    def _log_normalize(self, num: float, log_eps: float = 1e-8) -> float:
+        '''
+        Normalizes a number via taking the natural log of its absolute value, then reapplying the sign of the original number.
+        This is necessary in order for MeZO to converge.
+        '''
+        if num != 0:
+            if num > 0:
+                num = math.log(num)
+            else:
+                num = -1 * math.log(abs(num))
+        else:
+            num = math.log(self.eps)
+
+        return num
+
+    def _perturb_parameters(self, random_seed: int, scaling_factor: int) -> None:
+        '''
+        Randomly shifts the parameters to simulate movement in a random direction
+        in the dimensional space of the model.
+        '''
+        torch.manual_seed(random_seed)
+        for name, param in self.named_parameters_to_optim:
+            # Sample z
+            z = self._sample_z(param)
+            param.data = param.data + scaling_factor * z * self.eps
+
+    def _sample_z(self, param: nn.Parameter, sign: bool = False) -> torch.Tensor:
+        '''Samples a vector z.'''
+        z = torch.normal(
+                mean=0,
+                std=1,
+                size=param.data.size(),
+                device=param.data.device,
+                dtype=param.data.dtype
+        )
+        if sign:
+            z = z.sign()
+        return z
 
 # Adapted from https://github.com/princeton-nlp/MeZO/blob/main/large_models/trainer.py
 class MeZOTrainer(Trainer):
@@ -80,10 +206,13 @@ class MeZOTrainer(Trainer):
         lp_early_stopping,
         **kwargs
     ) -> None:
+
         self.use_mezo = use_mezo
         self.zo_eps = zo_eps
         self.linear_probing = linear_probing
         self.lp_early_stopping = lp_early_stopping
+        self.zo_random_seed = None
+    
         super().__init__(**kwargs)
 
     def _inner_training_loop(
@@ -211,6 +340,11 @@ class MeZOTrainer(Trainer):
 
         # Check if saved optimizer or scheduler states exist
         self._load_optimizer_and_scheduler(resume_from_checkpoint)
+
+        if self.use_mezo:
+            self.mezo = MeZOOptimizer(self, self.model)
+        else:
+            self.mezo = None
 
         # important: at this point:
         # self.model         is the Transformers Model
@@ -371,7 +505,7 @@ class MeZOTrainer(Trainer):
                 with self.accelerator.accumulate(model):
                     # MeZO added: estimate gradient
                     if self.use_mezo:
-                        tr_loss_step = self.zo_step(model, inputs)
+                        tr_loss_step = self.mezo.step(inputs)
                     else:
                         tr_loss_step = self.training_step(model, inputs)
 
@@ -397,7 +531,7 @@ class MeZOTrainer(Trainer):
                 ):
                     # MeZO added: update model with the estimated gradient
                     if self.use_mezo:
-                        self.zo_update(model)
+                        self.mezo.update()
                     else:
                         # Gradient clipping
                         if args.max_grad_norm is not None and args.max_grad_norm > 0:
@@ -533,97 +667,6 @@ class MeZOTrainer(Trainer):
         self.control = self.callback_handler.on_train_end(args, self.state, self.control)
 
         return TrainOutput(self.state.global_step, train_loss, metrics)
-    
-    ############## MeZO ##############
-
-    def zo_perturb_parameters(self, random_seed: t.Optional[int] = None, scaling_factor: float = 1) -> None:
-        """
-        Perturb the parameters with random vector z.
-        Input:
-        - random_seed: random seed for MeZO in-place perturbation (if it's None, we will use self.zo_random_seed)
-        - scaling_factor: theta = theta + scaling_factor * z * eps
-        """
-
-        # Set the random seed to ensure that we sample the same z for perturbation/update
-        torch.manual_seed(random_seed if random_seed is not None else self.zo_random_seed)
-
-        for name, param in self.named_parameters_to_optim:
-            z = torch.normal(
-                mean=0,
-                std=1,
-                size=param.data.size(),
-                device=param.data.device,
-                dtype=param.data.dtype
-            )
-            param.data = param.data + scaling_factor * z * self.zo_eps
-
-    def zo_forward(self, model: nn.Module, inputs: t.Any) -> torch.tensor:
-        """
-        Get (no gradient) loss from the model. Dropout is turned off too.
-        """
-        model.eval()
-        
-        with torch.inference_mode():
-            inputs = self._prepare_inputs(inputs)
-            with self.compute_loss_context_manager():
-                loss = self.compute_loss(model, inputs)
-            if self.args.n_gpu > 1:
-                # Warning: this is copied from the original HF Trainer. Untested.
-                loss = loss.mean() # mean() to average on multi-gpu parallel training
-            
-        return loss.detach()
-    
-    def zo_step(self, model: nn.Module, inputs: t.Any):
-        """
-        Estimate gradient by MeZO. Return the loss from f(theta + z)
-        """
-        # No gradient accumulation support
-        assert self.args.gradient_accumulation_steps == 1, "MeZO does not support gradient_accumulation_steps > 1"
-        args = self.args
-
-        # What parameters to optimize
-        self.named_parameters_to_optim = []
-        for name, param in model.named_parameters():
-            if param.requires_grad:
-                self.named_parameters_to_optim.append((name, param))
-
-        # Sample the random seed for sampling z
-        self.zo_random_seed = np.random.randint(1000000000)
-
-        # First function evaluation
-        self.zo_perturb_parameters(scaling_factor=1)
-        loss1 = self.zo_forward(model, inputs)
-
-        # Second function evaluation
-        self.zo_perturb_parameters(scaling_factor=-2)
-        loss2 = self.zo_forward(model, inputs)
-
-        self.projected_grad = ((loss1 - loss2) / (2 * self.zo_eps)).item()
-
-        # Reset model back to its parameters at start of step
-        self.zo_perturb_parameters(scaling_factor=1)
-
-        return loss1
-    
-    def zo_update(self, model):
-        """
-        Update the parameters with the estimated gradients.
-        """
-        args = self.args
-        IGNORE_DECAY_PARAMS = ["bias", "layer_norm", "layernorm"]
-
-        # Reset the random seed for sampling zs
-        torch.manual_seed(self.zo_random_seed)
-
-        for name, param in self.named_parameters_to_optim:
-            # Resample z
-            z = torch.normal(mean=0, std=1, size=param.data.size(), device=param.data.device, dtype=param.data.dtype)
-            if _list_not_in_name(search_terms=IGNORE_DECAY_PARAMS, name=name):
-                param.data = param.data - self._get_learning_rate() * (self.projected_grad * z + args.weight_decay * param.data)
-            else:
-                param.data = param.data - self._get_learning_rate() * (self.projected_grad * z)
-        
-        self.lr_scheduler.step()
 
 ## LINEAR PROBING
 def _linear_probe(model: PreTrainedModel, dataloader: DataLoader, args: TrainingArguments) -> None:
